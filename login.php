91cfdc5d67614b742202e0c345a5a621e9d70842
João Gustavo <92559361+Guga1010@users.noreply.github.com>
2023-01-24 14:07:51 -0300

    Update login.php

--- login.php
+++ login.php
@@ -9,14 +9,23 @@
 		
 		include "conexao.inc";
 		
-		$sql = "SELECT * FROM tb_login WHERE login = '$vlogin' AND senha = '$vsenha'";
+		$sql = "SELECT * FROM tb_login WHERE login = '$vlogin'";
 		$res = mysqli_query($con,$sql);
 		$lin = mysqli_affected_rows($con);
 		
 		if($lin == 1){
-			echo "Login realizado com sucesso!";
+			$sql = "SELECT * FROM tb_login WHERE login = '$vlogin' AND senha = $vsenha";
+			$res = mysqli_query($con,$sql);
+			$lin = mysqli_affected_rows($con);
+		
+			if($lin == 1){
+				echo "Login realizado com sucesso!";
+			}else{
+				echo "Senha incorreta<br/>";
+				echo "<a href='form_login.html'>Tentar novamente</a>";
+			}
 		}else{
-			echo "Falha no login<br/>";
+			echo "Login incorreto ou inexistente<br/>";
 			echo "<a href='form_login.html'>Tentar novamente</a>";
 		}
 		
