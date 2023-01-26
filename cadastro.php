4a1083194cb862c1a659453267ce8db29e5e8881
João Gustavo <92559361+Guga1010@users.noreply.github.com>
2023-01-26 12:25:28 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -6,6 +6,7 @@
 
 		$vlogin = $_POST['log_cad'];
 		$vsenha = $_POST['sen_cad'];
+		$vemail = $_POST['ema_cad'];
 		
 		include "conexao.inc";
 		
@@ -13,19 +14,30 @@
 		$res = mysqli_query($con,$sql);
 		$lin = mysqli_affected_rows($con);
 		
-		if($lin == 1){
-			echo "O nome de usuário digitado já existe<br/>";
+		if($lin > 0 ){
+			echo "Uma conta já existe com esse nome<br/>";
 			echo "<a href='form_cad.html'>Tentar outro nome</a>";
 		}else{
-			$sql = "INSERT INTO tb_login VALUES (NULL,'$vlogin','$vsenha')";
-			mysqli_query($con,$sql);
+			
+			$sql = "SELECT * FROM tb_login WHERE email = '$vemail'";
+			$res = mysqli_query($con,$sql);
 			$lin = mysqli_affected_rows($con);
-
-			if($lin == 1){
-				echo "Cadastrado com sucesso!<br/>";
-				echo "<a href='form_login.html'>Fazer login</a>";
+			
+			if($lin > 0 ){
+				echo "Uma conta já existe com esse email<br/>";
+				echo "<a href='form_cad.html'>Tentar outro email</a>";
 			}else{
-				echo "Falha no cadastro";
+			
+				$sql = "INSERT INTO tb_login VALUES (NULL,'$vlogin','$vsenha','$vemail')";
+				mysqli_query($con,$sql);
+				$lin = mysqli_affected_rows($con);
+
+				if($lin == 1){
+					echo "Cadastrado com sucesso!<br/>";
+					echo "<a href='form_login.html'>Fazer login</a>";
+				}else{
+					echo "Falha no cadastro";
+				}
 			}
 		}
 		mysqli_close($con);
